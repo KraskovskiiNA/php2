c43abf11317af8fbf0fef2fb2a80433def3bec06
KraskovskiiNA <58112849+KraskovskiiNA@users.noreply.github.com>
2020-09-21 03:38:58 +0300

    4.1

--- models/model.php
+++ models/model.php
@@ -50,18 +50,16 @@ abstract class Model
 
     public function update()
     {
-        $fields = [];
         $params = [];
         foreach ($this as $fieldName => $value) {
-            $fields[] = $fieldName;
-            $params[":{$fieldName}"] = $value;
+            $params[] = "`{$fieldName}`" . ' = ' . "'{$value}'";
         }  
-        
+    
         $sql = sprintf(
-            "UPDATE %s SET %s = %s WHERE `id` = $this->id ",
+            "UPDATE %s SET %s WHERE %s .`id` = $this->id ",
             static::getTableName(),
-            implode(',', $fields),
-            implode(',', $params)
+            implode(',', $params),
+            static::getTableName()
         );
         
         static::getDB()->execute($sql, $params);
